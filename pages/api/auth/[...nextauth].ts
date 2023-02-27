5514ddec9f5d155b360b7b8fe15518b18d1ab160
Raj kumar <glrajv369@gmail.com>
2023-02-27 10:56:38 +0530

    next auth configuration

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../prisma/client";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+})
