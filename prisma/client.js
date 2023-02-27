23d778a49b59a2a0bf855616a35a2ce058eeb637
Raj kumar <glrajv369@gmail.com>
2023-02-27 10:22:40 +0530

    initial prisma setup

---
+++ prisma/client.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+const client = globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = client
+}
+
+export default client
