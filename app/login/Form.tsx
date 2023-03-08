88dbcf36ca08a1275f3b9f91208296fa0bf2bdc8
Raj kumar <glrajv369@gmail.com>
2023-03-08 09:38:26 +0530

    react-hook-form integration

--- app/login/Form.tsx
+++ app/login/Form.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import EyeClose from '@/svg/common/eye-close.svg';
 import EyeOpen from '@/svg/common/eye-open.svg';
 
+type dataType = { email: string, password: string }
+
 function Form() {
+  const { register, formState: { errors }, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
   const [showPass, setShowPass] = useState(false)
 
   const updateShowPass = (e: React.FormEvent<HTMLButtonElement>) => {
@@ -12,37 +21,71 @@ function Form() {
     setShowPass(p => !p)
   }
 
+  const onSubmit = (data: dataType) => {
+    console.log(data)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4">
         <label htmlFor="login-email">Email</label>
         <input
+          autoFocus
           id="login-email"
           type="email"
-          name="email"
           placeholder="Enter your email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "Enter a valid email"
+            },
+          })}
         />
+
+        {
+          errors.email &&
+          <div className="mt-0.5 text-xs text-red-600">
+            {errors.email.message}
+          </div>
+        }
       </div>
 
-      <div className="mb-4 relative">
+      <div className="mb-4">
         <label htmlFor="login-password">Password</label>
-        <input
-          id="login-password"
-          type={showPass ? "text" : "password"}
-          name="password"
-          placeholder="Enter your password"
-          className='pr-9'
-        />
-        <button
-          onClick={updateShowPass}
-          className="px-0 absolute bottom-1 right-2"
-        >
-          {
-            showPass
-              ? <EyeOpen className="w-5 h-5" />
-              : <EyeClose className="w-5 h-5" />
-          }
-        </button>
+        <div className='relative'>
+          <input
+            id="login-password"
+            type={showPass ? "text" : "password"}
+            placeholder="Enter your password"
+            className='pr-9'
+            {...register("password", {
+              required: "Password is required",
+              pattern: {
+                value: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                message: "Password must be strong"
+              }
+            })}
+          />
+          <button
+            onClick={updateShowPass}
+            className="px-0 absolute bottom-1 right-2"
+            type="button"
+          >
+            {
+              showPass
+                ? <EyeOpen className="w-5 h-5" />
+                : <EyeClose className="w-5 h-5" />
+            }
+          </button>
+        </div>
+
+        {
+          errors.password &&
+          <div className="mt-0.5 text-xs text-red-600">
+            {errors.password.message}
+          </div>
+        }
       </div>
 
       <button
--- app/signup/Form.tsx
+++ app/signup/Form.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import EyeClose from '@/svg/common/eye-close.svg';
 import EyeOpen from '@/svg/common/eye-open.svg';
 
+type dataType = { email: string, password: string }
+
 function Form() {
+  const { register, formState: { errors }, handleSubmit } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  })
   const [showPass, setShowPass] = useState(false)
 
   const updateShowPass = (e: React.FormEvent<HTMLButtonElement>) => {
@@ -12,16 +22,33 @@ function Form() {
     setShowPass(p => !p)
   }
 
+  const onSubmit: SubmitHandler<dataType> = data => {
+    console.log(data)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4">
         <label htmlFor="signup-name">Name</label>
         <input
           id="signup-name"
           type="text"
-          name="name"
           placeholder="Enter your name"
+          {...register("name", {
+            required: "Name is required",
+            minLength: {
+              value: 3,
+              message: "Name should be atleast 3 characters"
+            }
+          })}
         />
+
+        {
+          errors.name &&
+          <div className="mt-0.5 text-xs text-red-600">
+            {errors.name.message}
+          </div>
+        }
       </div>
 
       <div className="mb-4">
@@ -29,30 +56,59 @@ function Form() {
         <input
           id="signup-email"
           type="email"
-          name="email"
           placeholder="Enter your email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "Enter a valid email"
+            },
+          })}
         />
+
+        {
+          errors.email &&
+          <div className="mt-0.5 text-xs text-red-600">
+            {errors.email.message}
+          </div>
+        }
       </div>
 
-      <div className="mb-4 relative">
+      <div className="mb-4">
         <label htmlFor="signup-password">Password</label>
-        <input
-          id="signup-password"
-          type={showPass ? "text" : "password"}
-          name="password"
-          placeholder="Enter your password"
-          className='pr-9'
-        />
-        <button
-          onClick={updateShowPass}
-          className="px-0 absolute bottom-1 right-2"
-        >
-          {
-            showPass
-              ? <EyeOpen className="w-5 h-5" />
-              : <EyeClose className="w-5 h-5" />
-          }
-        </button>
+        <div className='relative'>
+          <input
+            id="signup-password"
+            type={showPass ? "text" : "password"}
+            placeholder="Enter your password"
+            className='pr-9'
+            {...register("password", {
+              required: "Password is required",
+              pattern: {
+                value: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                message: "Password must be strong"
+              }
+            })}
+          />
+          <button
+            onClick={updateShowPass}
+            className="px-0 absolute bottom-1 right-2"
+            type="button"
+          >
+            {
+              showPass
+                ? <EyeOpen className="w-5 h-5" />
+                : <EyeClose className="w-5 h-5" />
+            }
+          </button>
+        </div>
+
+        {
+          errors.password &&
+          <div className="mt-0.5 text-xs text-red-600">
+            {errors.password.message}
+          </div>
+        }
       </div>
 
       <button
